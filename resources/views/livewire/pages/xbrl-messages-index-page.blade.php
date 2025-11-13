17e3abe4beb8f869e2c4bf1b7143b3106e99653a
Milo Visser <milovisser@live.nl>
2025-11-13 15:46:18 +0100

    Add message description column to XBRL messages index page for improved data visibility

--- resources/views/livewire/pages/xbrl-messages-index-page.blade.php
+++ resources/views/livewire/pages/xbrl-messages-index-page.blade.php
@@ -10,6 +10,7 @@
             <flux:table.columns>
                 <flux:table.column>{{ __('Gebruiker') }}</flux:table.column>
                 <flux:table.column>{{ __('Message UUID') }}</flux:table.column>
+                <flux:table.column>{{ __('Message Description') }}</flux:table.column>
                 <flux:table.column>{{ __('Message Type') }}</flux:table.column>
                 <flux:table.column>{{ __('Status') }}</flux:table.column>
                 <flux:table.column>{{ __('Created At') }}</flux:table.column>
@@ -20,6 +21,7 @@
                 <flux:table.row wire:key="xbrl-message-{{ $xbrlMessage->id }}">
                     <flux:table.cell>{{ $xbrlMessage->user->name }}</flux:table.cell>
                     <flux:table.cell>{{ $xbrlMessage->message_uuid }}</flux:table.cell>
+                    <flux:table.cell>{{ $xbrlMessage->message_description }}</flux:table.cell>
                     <flux:table.cell>{{ $xbrlMessage->message_type }}</flux:table.cell>
                     <flux:table.cell>{{ $xbrlMessage->status }}</flux:table.cell>
                     <flux:table.cell>{{ $xbrlMessage->created_at->format('d-m-Y H:i') }}</flux:table.cell>
